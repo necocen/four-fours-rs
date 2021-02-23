e0b31a1649b5626ad074963e6bc551588fcddc62
necocen <necocen@gmail.com>
2021-02-23 12:11:03 +0900

    数値トークンのデリミタがなかったので修正

--- src/search.rs
+++ src/search.rs
@@ -4,8 +4,13 @@ mod unary_op;
 
 pub type Value = f64;
 /// 演算子および桁トークン。
-/// `0xf0`より後の値は桁のために予約済みで、
-/// `t = 0xf0 + n (0 <= n < 10)`のとき、数字`n`を表す。
+/// `0xe0`より後の値は桁のために予約済みで、
+/// - `t = 0xe0 + n (0 <= n < 10)`
+/// または
+/// - `t = 0xf0 + n (0 <= n < 10)`
+///
+/// のとき、数字`n`を表す。
+/// `0xeX`は先頭桁を示し、`0xfX`は残りの桁を示す。
 pub type Token = u8;
 use std::{
     collections::{hash_map::Entry, HashMap},
