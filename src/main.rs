fe585c02badc4f0aba3ebd32da5732f962c777e7
necocen <necocen@gmail.com>
2021-02-23 14:46:52 +0900

    add comments

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ use search::{BinaryOp, Equation, Searcher, UnaryOp};
 use std::collections::{hash_map::Entry, HashMap};
 
 fn main() {
-    println!("Hello, world!");
-
+    // unary ops
     let negate = UnaryOp::new(0x00, 2, |v| Some(-v));
     let sqrt = UnaryOp::new(0x01, 4, |v| if v < 0f64 { None } else { Some(v.sqrt()) });
+    // binary ops
     let add = BinaryOp::new(0x10, 1, |v1, v2| Some(v1 + v2));
     let sub = BinaryOp::new(0x11, 2, |v1, v2| Some(v1 - v2));
     let mul = BinaryOp::new(0x12, 3, |v1, v2| Some(v1 * v2));
@@ -18,12 +18,14 @@ fn main() {
         |v1, v2| if v2 == 0f64 { None } else { Some(v1 / v2) },
     );
     let pow = BinaryOp::new(0x14, 6, |v1, v2| Some(v1.powf(v2)));
+
+    // 探索
     let searcher = Searcher::new(vec![negate, sqrt], vec![add, sub, mul, div, pow]);
     let knowledge = searcher.knowledge("4444");
 
+    // 整数値のみを出力する
     let mut results = HashMap::<i32, Equation>::new();
     for (_, e) in knowledge {
-        // 整数値のみ出力
         if e.value >= 0f64 && e.value < 2000f64 && e.value.fract().abs() < 1e-9 {
             let rounded = e.value.round() as i32;
             match results.entry(rounded) {
@@ -39,6 +41,7 @@ fn main() {
         }
     }
 
+    // 結果表示
     let printer = Printer::new(vec![], vec![], "(", ")", " = ");
 
     for n in 0..=1000 {
